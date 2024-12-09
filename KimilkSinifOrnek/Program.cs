bff4124107092a8f15081e2da265515f1b7d770a
emirozkul <emirrozkul@gmail.com>
2024-12-09 20:43:12 +0300

    Metotlar ve Sınıflar pekiştirildi. Yeni bölüme Geçildi.

---
+++ KimilkSinifOrnek/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using KimilkSinifOrnek;
+
+namespace KimlikSinifOrnek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kimlik kml = new Kimlik();
+
+            kml.AD = "emir";
+            kml.SOYAD = "özkul";
+            kml.MEMLEKET = "kayseri";
+            kml.YAS = 25;
+            kml.Cinsiyet = 'E';
+
+            Console.WriteLine(kml.AD);
+            Console.WriteLine(kml.SOYAD);
+            Console.WriteLine(kml.MEMLEKET);
+            Console.WriteLine(kml.YAS);
+            Console.WriteLine(kml.Cinsiyet);
+
+            Console.Read();
+
+        }
+    }
+}
---
+++ KimilkSinifOrnek/kimlik.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace KimilkSinifOrnek
+{
+    internal class Kimlik
+    {
+
+        string ad;
+        string soyad;
+        string memleket;
+        int yas;
+        char cinsiyet;
+
+        public string AD
+        {
+            get { return ad; }
+            set { ad = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(value); }
+        }
+
+        public string SOYAD
+        {
+            get { return soyad; }
+            set { soyad = value.ToUpper(); }
+        }
+
+        public string MEMLEKET
+        {
+            get { return memleket; }
+            set { memleket = value.ToUpper(); }
+
+        }
+
+        public int YAS
+        {
+            get { return yas; }
+            set { yas = Math.Abs(value); }
+        }
+
+        public char Cinsiyet
+        {
+            get { return cinsiyet; }
+            set { cinsiyet = value; }
+        }
+
+        public Kimlik() 
+        {
+            ad = "";
+            soyad = "";
+            yas = 0;
+            cinsiyet = 'B';
+            memleket = "belirsiz";
+        }
+
+    }
+}
